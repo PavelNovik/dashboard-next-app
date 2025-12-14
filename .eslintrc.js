8300fe24b1687c7bffad7385206f6fccbcd19f6e
Pavel Novik <brn2befree@gmail.com>
2025-12-14 21:09:20 +0300

    add prettier in proj

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier'
+    ],
+    plugins: ['react', '@typescript-eslint'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn'
+    }
+}
