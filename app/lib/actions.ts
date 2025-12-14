ca6bb85ab40b31a5d0aa017285a736530309082d
Pavel Novik <brn2befree@gmail.com>
2025-12-15 02:16:49 +0300

    add validation and schema

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,1 +1,24 @@
 'use server'
+
+import { z } from 'zod'
+
+const FormSchema = z.object({
+  id: z.string(),
+  customer_id: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  }
+  // This form data will be printed in the terminal, not the browser console
+
+  console.log(rawFormData)
+}
