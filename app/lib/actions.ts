d4f0556fde4f85fc233749e6bcaaba1e65b2fbe4
Pavel Novik <brn2befree@gmail.com>
2025-12-15 02:28:27 +0300

    add command to sql to incert our data in database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,13 @@
 'use server'
 
 import { z } from 'zod'
+import postgres from 'postgres'
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
 const FormSchema = z.object({
   id: z.string(),
-  customer_id: z.string(),
+  customerId: z.string(),
   amount: z.coerce.number(),
   status: z.enum(['pending', 'paid']),
   date: z.string(),
@@ -13,12 +16,19 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  }
-  // This form data will be printed in the terminal, not the browser console
+  })
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0]
 
-  console.log(rawFormData)
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`
+
+  // This form data will be printed in the terminal, not the browser console
+  // console.log(rawFormData)
 }
