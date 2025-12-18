ecd37d58456044d3f201f3acab3f982c8fcbdc7f
Pavel Novik <brn2befree@gmail.com>
2025-12-18 17:00:01 +0300

    add validation on edit invoice form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,12 +70,19 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    }
+  }
+  const { customerId, amount, status } = validatedFields.data
 
   const amountInCents = amount * 100
 
@@ -95,12 +102,12 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  // try {
-  throw new Error('Failed to delete Invoice.')
-  await sql`DELETE FROM invoices WHERE id = ${id}`
-  // } catch (error) {
-  //   console.error(error)
-  //   return { message: 'Database Error: Failed to Delete Invoice.' }
-  // }
+  try {
+    // throw new Error('Failed to delete Invoice.')
+    await sql`DELETE FROM invoices WHERE id = ${id}`
+  } catch (error) {
+    console.error(error)
+    return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
   revalidatePath('/dashboard/invoices')
 }
