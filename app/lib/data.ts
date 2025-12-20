9a013a2ad9e881ff3005f357aafec5ec442717cc
Pavel Novik <brn2befree@gmail.com>
2025-12-20 17:13:25 +0300

    n 2 ch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,13 @@
 import postgres from 'postgres'
-import {CustomerField, CustomersTableType, InvoiceForm, InvoicesTable, LatestInvoiceRaw, Revenue,} from './definitions'
-import {formatCurrency} from './utils'
+import {
+  CustomerField,
+  CustomersTableType,
+  InvoiceForm,
+  InvoicesTable,
+  LatestInvoiceRaw,
+  Revenue,
+} from './definitions'
+import { formatCurrency } from './utils'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
